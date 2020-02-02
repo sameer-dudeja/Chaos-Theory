557d6ba3eb3b68b8c178bc8c409aad53dbdd035a
Sameer Dudeja <dsameer33@gmail.com>
2020-02-03 04:20:45 +0530

    lorenz_theory_1

--- lorenz_chaos.m
+++ lorenz_chaos.m
@@ -1,5 +1,5 @@
 clear all, close all, clc
-beta = [10;28;8/3];
+beta = [9;28;8/3];
 x0 = [0; 1;20];
 dt = 0.001;
 
@@ -11,3 +11,4 @@ options = odeset('RelTol',1e-12,'AbsTol',1e-12*ones(1,3));
 plot3(x(:,1),x(:,2),x(:,3),'w','LineWidth',1.5);
 set(gca,'color','k','xcolor','w','ycolor','w','zcolor','w');
 set(gcf,'color','k');
+colormap jet
